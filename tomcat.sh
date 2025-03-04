734fdae7848d10a81937f03d4e70a43e6ddba928
chakaliabhishek <princeabhi2580@gmail.com>
2025-03-04 18:03:17 +0530

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -1,4 +1,4 @@
-
+yum install java-17-amazon-corretto -y
 wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.96/bin/apache-tomcat-9.0.99.tar.gz
 tar -zxvf apache-tomcat-9.0.99.tar.gz
 sed -i '56  a\<role rolename="manager-gui"/>' apache-tomcat-9.0.99/conf/tomcat-users.xml
